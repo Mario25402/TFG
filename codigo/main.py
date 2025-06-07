6cec273dd681dfc58509ee3be47a464707fe78b5
Mario <mario_25_4@outlook.com>
2025-06-07 13:26:45 +0200

    Adición cuatrimestres

--- codigo/main.py
+++ codigo/main.py
@@ -6,10 +6,22 @@ from gruposAsignatura import *
 
 def main():
     sys.setrecursionlimit(10000)
-    matHor = MatriculaHorario("./jesus/matricula.csv", "./jesus/horariosgiit1.csv")#, "./jesus/horariosgiit2.csv")
 
-    clases = gruposAsignatura("./jesus/horariosgiit1.csv", "./jesus/horariosgiit2.csv")
+    ###
+    # Primer Cuatrimestre
+
+    matHor = MatriculaHorario("./jesus/matricula.csv", "./jesus/horariosgiit1.csv")
+    clases = gruposAsignatura("./jesus/horariosgiit1.csv")
+
     clases.getAulasRellenas(matHor.combinaciones)
+
+    ###
+    # Segundo Cuatrimestre
+
+    #matHor = MatriculaHorario("./jesus/matricula.csv", "./jesus/horariosgiit2.csv")
+    #clases = gruposAsignatura("./jesus/horariosgiit2.csv")
+
+    #clases.getAulasRellenas(matHor.combinaciones)
 
 ##########
 
